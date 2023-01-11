5fd216a720a68f8dbbb9c170e146b8c3a81f925d
Gabriel Vial <gabriel.vial@globant.com>
2023-01-11 15:42:03 -0300

    feat: update stuff to expose /pwt in pages-map

--- next.config.js
+++ next.config.js
@@ -1,6 +1,32 @@
 /** @type {import('next').NextConfig} */
+
+const NextFederationPlugin = require('@module-federation/nextjs-mf');
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack(config, options) {
+    const { webpack } = options;
+    Object.assign(config.experiments, { topLevelAwait: true });
+    if (!options.isServer) {
+      //config.cache=false
+      config.plugins.push(
+        new NextFederationPlugin({
+          name: "pwt",
+          remotes: {
+            home: `home@http://localhost:3001/_next/static/chunks/remoteEntry.js`,
+          },
+          filename: "static/chunks/remoteEntry.js",
+          exposes: {
+            "./pwt": "./pages/pwt",
+            "./pages-map": "./pages-map.js",
+          },
+          shared: {},
+        })
+      );
+    }
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
